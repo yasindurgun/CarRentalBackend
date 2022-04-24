aff5129d459e200a323fa6c43505f60d1a27941a
yasindurgun <yasin.durgun52@gmail.com>
2022-04-25 02:38:57 +0300

    Create RentalManager business.

---
+++ Business/Concrete/RentalManager.cs
@@ -0,0 +1,34 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class RentalManager : IRentalService
+    {
+        private readonly IRentalDal _rentalDal;
+        public RentalManager(IRentalDal rentalDal)
+        {
+            _rentalDal = rentalDal;
+        }
+        public IResult AddRental(Rental rental)
+        {
+            
+            if (rental.ReturnDate > DateTime.Now)
+            {
+                return new ErrorResult();
+            }
+            else
+            {
+                _rentalDal.Add(rental);
+                return new SuccessResult();
+            }
+        }
+    }
+}
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -16,54 +16,70 @@ namespace ConsoleUI
 
             //BrandOperations();
 
-            CarManager carManager = new CarManager(new EfCarDal());
+            //CarManager carManager = new CarManager(new EfCarDal());
 
-            foreach (var item in carManager.GetCarDetails())
+            //GetCarDetailsOperation();
+
+            RentalManager rentalManager = new RentalManager(new EfRentalDal());
+            Rental rental = new Rental()
             {
-                Console.WriteLine($"Car Name: {item.BrandName}");
-                Console.WriteLine($"Description: {item.Description}");
-                Console.WriteLine($"Daily Price: {item.DailyPrice}");
-                Console.WriteLine($"Color: {item.ColorName}");
-                Console.WriteLine();
-                Console.WriteLine("**********\n");
-            }
-
-            Console.ReadLine();
-        }
-
-        private static void BrandOperations()
-        {
-            BrandManager brandManager = new BrandManager(new EfBrandDal());
-
-            var brand = brandManager.GetById(55);
-
-            Console.WriteLine(brand.BrandName);
-        }
-
-        private static void AddCarWithCarManager()
-        {
-            CarManager carManager = new CarManager(new EfCarDal());
-            Car car = new Car
-            {
-                BrandId = 2,
-                ColorId = 3,
-                Description = "White BMW",
-                DailyPrice = 0,
-                ModelYear = "2021"
+                CarId = 1,
+                CustomerId = 1,
+                RentDate = DateTime.Now.AddDays(-10),
+                ReturnDate = DateTime.Now.AddDays(-5),
             };
-
-            carManager.AddCar(car);
+            Console.WriteLine("deneme1");
+            rentalManager.AddRental(rental);
+            Console.WriteLine("deneme2");
+            Console.ReadKey();
         }
 
-        private static CarManager InMemoryCarManager()
-        {
-            CarManager carManager = new CarManager(new InMemoryCarDal());
-            foreach (var item in carManager.GetAll())
-            {
-                Console.WriteLine($"Car Description: {item.Description}");
-            }
+        //private static void GetCarDetailsOperation()
+        //{
+        //    foreach (var item in carManager.GetCarDetails())
+        //    {
+        //        Console.WriteLine($"Car Name: {item.BrandName}");
+        //        Console.WriteLine($"Description: {item.Description}");
+        //        Console.WriteLine($"Daily Price: {item.DailyPrice}");
+        //        Console.WriteLine($"Color: {item.ColorName}");
+        //        Console.WriteLine();
+        //        Console.WriteLine("**********\n");
+        //    }
+        //}
 
-            return carManager;
-        }
+        //private static void BrandOperations()
+        //{
+        //    BrandManager brandManager = new BrandManager(new EfBrandDal());
+
+        //    var brand = brandManager.GetById(55);
+
+        //    Console.WriteLine(brand.BrandName);
+        //}
+
+        //private static void AddCarWithCarManager()
+        //{
+        //    CarManager carManager = new CarManager(new EfCarDal());
+        //    Car car = new Car
+        //    {
+        //        BrandId = 2,
+        //        ColorId = 3,
+        //        Description = "White BMW",
+        //        DailyPrice = 0,
+        //        ModelYear = "2021"
+        //    };
+
+        //    carManager.AddCar(car);
+        //}
+
+        //private static CarManager InMemoryCarManager()
+        //{
+        //    CarManager carManager = new CarManager(new InMemoryCarDal());
+        //    foreach (var item in carManager.GetAll())
+        //    {
+        //        Console.WriteLine($"Car Description: {item.Description}");
+        //    }
+
+        //    return carManager;
+        //}
     }
 }
